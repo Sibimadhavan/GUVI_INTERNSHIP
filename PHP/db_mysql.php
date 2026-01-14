c7ecb395988dadbb571d66ab91d594b2d0c3ed4c
Sibimadhavan <148133428+Sibimadhavan@users.noreply.github.com>
2026-01-14 13:13:34 +0530

    Update db_mysql.php

--- PHP/db_mysql.php
+++ PHP/db_mysql.php
@@ -1,10 +1,11 @@
 <?php
-$conn = new mysqli("127.0.0.1", "root", "guvi1234", "guvi", 3306);
+$conn = new mysqli("guvi.cb0a2uom643v.ap-south-1.rds.amazonaws.com", "root", "guvi1234", "guvi", 3306);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 
 ?>
+
 
 
